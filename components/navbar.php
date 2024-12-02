f8cefb5a09f969d8958a6ef6657cceecefc37e83
Computarfant <lennartbirk@gmail.com>
2024-12-02 23:17:51 +0100

    routing for whole projectfolder in htdocs

--- components/navbar.php
+++ components/navbar.php
@@ -13,17 +13,17 @@ if (isset($_SESSION['rolleId'])) {
 <div class="navbar">
     <?php if ($rolleId == 1): ?>
         <!-- User Navbar -->
-        <a href="/views/bookingLogic/booking.php">Booking</a>
-        <a href="/views/userLogic/profil.php">Din Profil</a>
-        <a href="/views/authentication/logout.php">logout</a>
+        <a href="/IS115Prosjekt/views/bookingLogic/booking.php">Booking</a>
+        <a href="/IS115Prosjekt/views/userLogic/profil.php">Din Profil</a>
+        <a href="/IS115Prosjekt/views/authentication/logout.php">logout</a>
     <?php elseif ($rolleId == 2): ?>
         <!-- Admin Navbar -->
-        <a href="/views/userLogic/logOversikt.php">Log</a>
-        <a href="/views/userLogic/brukerOversikt.php">Bruker oversikt</a>
-        <a href="/views/roomAdminLogic/romOversikt.php">Rom administrering</a>
-        <a href="/views/bookingLogic/booking.php">Booking</a>
-        <a href="/views/authentication/logout.php">logout</a>
-        <a href="/views/userLogic/profil.php">Din Profil</a>
+        <a href="/IS115Prosjekt/views/userLogic/logOversikt.php">Log</a>
+        <a href="/IS115Prosjekt/views/userLogic/brukerOversikt.php">Bruker oversikt</a>
+        <a href="/IS115Prosjekt/views/roomAdminLogic/romOversikt.php">Rom administrering</a>
+        <a href="/IS115Prosjekt/views/bookingLogic/booking.php">Booking</a>
+        <a href="/IS115Prosjekt/views/userLogic/profil.php">Din Profil</a>
+        <a href="/IS115Prosjekt/views/authentication/logout.php">logout</a>
     <?php else: ?>
         <!-- Optionally, handle unknown roles here -->
         <p>Unauthorized role</p>
